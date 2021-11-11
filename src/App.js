95f81406e310947932b29d6044b698b63845fd60
mramirez0610 <ramimarc@iu.edu>
2021-11-11 14:14:41 -0500

    basic-push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AlbumView from "./components/albumView";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1>sup marco</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <AlbumView />
+        {/* 
+        
+        questions to ask:
+
+        -are mui core / material the same?
+        
+        -why does it return an empty array first?
+             - should i do the same thing as in my calendar?
+
+        */}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/albumView.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+export default function AlbumView() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("data/listOfAlbums.json")
+      .then((res) => res.json())
+      .then((data) => {
+        //assigns our data from json into an actual variable
+        setAlbums(data);
+        
+      });
+  }, []);
+
+  console.log(albums);
+  return <div> </div>;
+}
