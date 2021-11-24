b1ac537c176d5aed3713a8ba7839f4ff6756518d
marco <ramimarc@iu.edu>
2021-11-24 15:42:44 -0500

    finally, MUI component is functional -- all necessary data is on page -- animations next step

--- src/components/listComp/trackListing.js
+++ src/components/listComp/trackListing.js
@@ -1,5 +1,4 @@
 import React from "react";
-import RowInfo from "./rowDetails";
 import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -8,24 +7,7 @@ import { FixedSizeList } from 'react-window';
 
 
 export default function TrackListing(props){
-
   const trackList = props.selectedAl.trackList;
-  console.log(trackList.length);
-  
-  const RowInfoThree = ({style, index}) => (
-    <div style={style} key={index}>
-      <p>{trackList[index]}</p>  
-    </div>
-  );
-
-  for(let i = 0; i < trackList.length; i++){
-    console.log(trackList[i])
-    var RowInfoTwo = ({index, style}) => (
-      <div style={style} key={index}>
-        <p>{props.selectedAl.trackList[i].songTitle}</p>  
-      </div>
-    );
-  }
 
   const Row = ({index , style}) => (
     <ListItem 
@@ -35,22 +17,13 @@ export default function TrackListing(props){
       disablePadding
     >
       <ListItemButton>
-        <ListItemText primary={`${RowInfoThree}`} />
+        <ListItemText primary={trackList[index].songTitle} />
         {/* 
           --trouble here. i am so so close. for some reason i can't 
           get this to go through every track list. i am soooooooooooooo 
           close to figuring this out
-
-          --also, length only works after i've saved the file, but when i
-          reload, there's an error. how funny
-
+          
           im assuming its because it's trying to read 0
-
-          <RowInfo 
-            track={props.selectedAl.trackList.songTitle} 
-          />
-        
-        </ListItemText>
         */}
       </ListItemButton>
     </ListItem>
@@ -65,7 +38,7 @@ export default function TrackListing(props){
     className="list" 
     height={400}
     width={350}
-    itemSize={46}
+    itemSize={45}
     itemCount={trackList.length}
     overscanCount={5}
   >
